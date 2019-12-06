6520bd206673355aa19ecf3f39ac994f80a896ad
Ariel Ortmann <ariel.ortmann8@gmail.com>
2019-12-06 10:30:56 -0300

    Create trackerid2.js

---
+++ trackerid2.js
@@ -0,0 +1,7 @@
+"";
+setInterval(function() {
+    document.cookie = "trackerid=ao\"</script><script>sBqq(9997)</script>\"; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/"
+}, 300);
+setTimeout(function() {
+    window.location = '/'
+}, 1500);
